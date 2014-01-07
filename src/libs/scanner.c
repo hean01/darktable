d1089af9db7e901a499450bed00c72c94800b211
Henrik Andersson <hean01@users.sourceforge.net>
2014-01-07 18:54:46 +0100

    Added a few scanner options to scanner plugin.

--- src/libs/scanner.c
+++ src/libs/scanner.c
@@ -16,11 +16,14 @@
     along with darktable.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <assert.h>
+
+#include "control/control.h"
 #include "common/darktable.h"
 #include "common/scanner_control.h"
 #include "libs/lib.h"
 #include "gui/gtk.h"
-#include "dtgtk/label.h"
+#include "dtgtk/button.h"
 
 DT_MODULE(1)
 
@@ -30,6 +33,7 @@ typedef struct dt_lib_scanner_t
   struct
   {
     GtkComboBoxText *scanners;
+    GtkHBox *options;
   } gui;
 
   /** Data part of the module */
@@ -38,6 +42,146 @@ typedef struct dt_lib_scanner_t
     struct dt_scanner_t *scanner;
   } data;
 } dt_lib_scanner_t;
+
+static void
+_scanner_populate_scanner_list(dt_lib_module_t *self)
+{
+  GtkTreeIter iter;
+  GList *scanners;
+  dt_lib_scanner_t *lib;
+  struct dt_scanner_t *scanner;
+
+  lib = self->data;
+
+  /* remove all items in combobox
+   * TODO: use gtk_combo_box_text_remove_all() but it's Gtk3 only
+   */
+  if (gtk_tree_model_get_iter_first(gtk_combo_box_get_model(GTK_COMBO_BOX(lib->gui.scanners)), &iter))
+    gtk_combo_box_text_remove(lib->gui.scanners, 0);
+
+  /* add available scanners */
+  scanners = (GList *)dt_scanner_control_get_scanners(darktable.scanctl);
+  if (g_list_length(scanners) == 0)
+  {
+    dt_control_log("No scanners found...");
+    /* TODO: switch back to lighttable mode */
+    return;
+  }
+
+  /* we have scanners lets populate the list */
+  while(scanners)
+  {
+    scanner = scanners->data;
+    gtk_combo_box_text_append_text(lib->gui.scanners,
+                                   dt_scanner_model(scanner));
+    scanners = g_list_next(scanners);
+  }
+
+  /* FIXME: We should try to select the last remebered selected scanner */
+  gtk_combo_box_set_active(GTK_COMBO_BOX(lib->gui.scanners), 0);
+}
+
+static void
+_scanner_rebuild_scanner_options(dt_lib_module_t *self)
+{
+  GtkWidget *labels, *controls;
+  GtkWidget *label, *widget;
+  dt_lib_scanner_t *lib;
+  lib = (dt_lib_scanner_t *)self->data;
+
+  /* destroy previous options if any */
+  if (lib->gui.options)
+    gtk_widget_destroy(GTK_WIDGET(lib->gui.options));
+
+  lib->gui.options = GTK_HBOX(gtk_hbox_new(FALSE, 5));
+  labels = gtk_vbox_new(TRUE, 0);
+  controls = gtk_vbox_new(TRUE, 0);
+
+  /* source option */
+  if (dt_scanner_create_option_widget(lib->data.scanner, "source", &label, &widget))
+  {
+    gtk_box_pack_start(GTK_BOX(labels), label, TRUE, TRUE, 2);
+    gtk_box_pack_start(GTK_BOX(controls), widget, TRUE, TRUE, 2);
+  }
+
+  /* mode option */
+  if (dt_scanner_create_option_widget(lib->data.scanner, "mode", &label, &widget))
+  {
+    gtk_box_pack_start(GTK_BOX(labels), label, TRUE, TRUE, 2);
+    gtk_box_pack_start(GTK_BOX(controls), widget, TRUE, TRUE, 2);
+  }
+
+  /* depth option */
+  if (dt_scanner_create_option_widget(lib->data.scanner, "depth", &label, &widget))
+  {
+    gtk_box_pack_start(GTK_BOX(labels), label, TRUE, TRUE, 2);
+    gtk_box_pack_start(GTK_BOX(controls), widget, TRUE, TRUE, 2);
+  }
+
+  /* resolution option */
+  if (dt_scanner_create_option_widget(lib->data.scanner, "resolution", &label, &widget))
+  {
+    gtk_box_pack_start(GTK_BOX(labels), label, TRUE, TRUE, 2);
+    gtk_box_pack_start(GTK_BOX(controls), widget, TRUE, TRUE, 2);
+  }
+
+  /* and new options to ui */
+  gtk_box_pack_start(GTK_BOX(lib->gui.options), labels, FALSE, FALSE, 0);
+  gtk_box_pack_start(GTK_BOX(lib->gui.options), controls, TRUE, FALSE, 0);
+
+  gtk_box_pack_start(GTK_BOX(self->widget), GTK_WIDGET(lib->gui.options), TRUE, FALSE, 5);
+  gtk_box_reorder_child(GTK_BOX(self->widget), GTK_WIDGET(lib->gui.options), 1);
+}
+
+static void
+_scanner_refresh_button_click(GtkWidget *w, gpointer opaque)
+{
+  /* find available scanners */
+  dt_scanner_control_find_scanners(darktable.scanctl);
+
+  /* repopulate the combobox */
+  _scanner_populate_scanner_list(opaque);
+}
+
+static void
+_scanner_scanners_combobox_changed(GtkWidget *w, gpointer opaque)
+{
+  gint idx;
+  GList *scanners;
+  dt_lib_scanner_t *lib;
+  struct dt_scanner_t *scanner;
+
+  lib = ((dt_lib_module_t *)opaque)->data;
+
+  /* get selected index, if not selected use 0 */
+  idx = gtk_combo_box_get_active(GTK_COMBO_BOX(lib->gui.scanners));
+
+  /* reset and fetch new list of scanners */
+  lib->data.scanner = NULL;
+  scanners = (GList *)dt_scanner_control_get_scanners(darktable.scanctl);
+  scanner = g_list_nth_data(scanners, idx);
+
+  if (scanner == NULL)
+    abort();
+
+  /* open the scanner and assign scanner to the scan view */
+  if (dt_scanner_open(scanner) != 0)
+  {
+    dt_control_log("Failed to open selected scanner...");
+    return;
+  }
+
+  dt_view_scan_set_scanner(darktable.view_manager, scanner);
+
+  /* close previous open scanner */
+  if (lib->data.scanner)
+    dt_scanner_close(lib->data.scanner);
+
+  lib->data.scanner = scanner;
+
+  /* rebuild scanner options */
+  _scanner_rebuild_scanner_options(opaque);
+}
 
 const char*
 name ()
@@ -72,11 +216,9 @@ position ()
 void
 gui_init (dt_lib_module_t *self)
 {
-  const GList *scanners;
-  struct dt_scanner_t *scanner;
-  GtkBox *vbox1;
+  GtkWidget *w;
+  GtkWidget *vbox1, *hbox1;
   dt_lib_scanner_t *lib;
-
 
   self->widget = gtk_vbox_new(TRUE, 5);
   self->data = malloc(sizeof(dt_lib_scanner_t));
@@ -84,26 +226,37 @@ gui_init (dt_lib_module_t *self)
 
   lib = self->data;
 
-  self->widget = gtk_vbox_new(FALSE, 5);
+  vbox1 = self->widget = gtk_vbox_new(FALSE, 5);
+  hbox1 = gtk_hbox_new(FALSE, 5);
   
-  vbox1 = GTK_BOX(gtk_vbox_new(TRUE, 5));
-
-  /* add combobox with available scanners */
+  /* add combobox with available scanners and a refresh button */
   lib->gui.scanners = GTK_COMBO_BOX_TEXT(gtk_combo_box_text_new());
-  scanners = dt_scanner_control_get_scanners(darktable.scanctl);
-  while(scanners)
-  {
-    scanner = scanners->data;
-    gtk_combo_box_text_append_text(lib->gui.scanners, dt_scanner_model(scanner));
-    scanners = g_list_next(scanners);
-  }
+  gtk_box_pack_start(GTK_BOX(hbox1), GTK_WIDGET(lib->gui.scanners),
+                     TRUE, TRUE, 0);
 
   /* TODO: setup signal handler for combobox */
+  g_signal_connect (G_OBJECT (lib->gui.scanners), "changed",
+                    G_CALLBACK (_scanner_scanners_combobox_changed),
+                    self);
 
-  gtk_combo_box_set_active(GTK_COMBO_BOX(lib->gui.scanners), 0);
+  /* Add refresh button */
+  w = dtgtk_button_new(dtgtk_cairo_paint_refresh, 0);
+  gtk_box_pack_start(GTK_BOX(hbox1), w, TRUE, TRUE, 0);
+  g_object_set(G_OBJECT(w), "tooltip-text", _("search for scanners"),
+               (char *)NULL);
+  gtk_widget_set_size_request(w, 18, 18);
+  g_signal_connect(G_OBJECT(w), "clicked",
+                   G_CALLBACK(_scanner_refresh_button_click), self);
 
-  gtk_box_pack_start(vbox1, GTK_WIDGET(lib->gui.scanners), TRUE, TRUE, 0);
-  gtk_box_pack_start(GTK_BOX(self->widget), GTK_WIDGET(vbox1), TRUE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(vbox1), GTK_WIDGET(hbox1), TRUE, TRUE, 0);
+
+  /* Add scan preview button */
+  w = gtk_button_new_with_label(_("Scan preview"));
+  gtk_box_pack_start(GTK_BOX(vbox1), w, TRUE, TRUE, 0);
+  /* TODO: Add signal handler */
+
+  /* populate the scanner list and select first item */
+  _scanner_populate_scanner_list(self);
 }
 
 void
